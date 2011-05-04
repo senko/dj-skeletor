799bd828dc16e8bafd036c923d69c695e6472361
Senko Rasic <senko.rasic@dobarkod.hr>
2011-05-04 17:25:27 +0200

    enable Django Debug Toolbar in development environment

--- settings/dev.py
+++ settings/dev.py
@@ -9,4 +9,16 @@ DATABASES = {
     }
 }
 
+# Django Debug Toolbar
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': lambda *args, **kwargs: True
+}
 

8cbecbc89f2e68256d226cf180395d1f03127187
Senko Rasic <senko.rasic@dobarkod.hr>
2011-05-04 17:06:53 +0200

    empty relocatable Django project

---
+++ settings/__init__.py
@@ -0,0 +1,14 @@
+# Create a separate local config for each of the environments the app will
+# run in. Store all the configs in git. To activate a particular configuration,
+# symlink settings/local.py to it. Do not store the symlink in the git repo.
+
+from .base import *
+
+try:
+    from .local import *
+except ImportError:
+    pass
+
+
+TEMPLATE_DEBUG = DEBUG
+
---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'skeletor.views.home', name='home'),
+    # url(r'^skeletor/', include('skeletor.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)

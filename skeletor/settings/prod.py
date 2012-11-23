4b830335e125a9fa9663e9a31ca11257394f1636
Senko Rasic <senko.rasic@dobarkod.hr>
2012-11-24 00:45:34 +0100

    settings.prod: try guessing the db settings with dj_database_url
    This makes Skeletor Just Work [TM] on Heroku.

--- skeletor/settings/prod.py
+++ skeletor/settings/prod.py
@@ -1,14 +1,23 @@
 from .base import *
 
-DEBUG = False
+DEBUG = TEMPLATE_DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+# Try to use DATABASE_URL environment variable if possible, otherwise fall back
+# to hardcoded values
+try:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+except ImportError:
+    DATABASES = {}
+
+if not DATABASES or not DATABASES.get('default'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': '',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}

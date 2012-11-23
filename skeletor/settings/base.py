086497599e54d493b71c2b44e2a6ddfb562aa8e3
Senko Rasic <senko.rasic@dobarkod.hr>
2012-11-24 00:44:49 +0100

    settings.base: use timezones by default

--- skeletor/settings/base.py
+++ skeletor/settings/base.py
@@ -38,6 +38,10 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+# If you set this to False, Django will treat all time values as local to
+# the specified timezone.
+USE_TZ = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ABS_PATH('media')

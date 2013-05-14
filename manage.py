ddba568457636da1f0183ce34c1ed34758256c81
Senko Rasic <senko.rasic@dobarkod.hr>
2013-05-14 19:39:40 +0200

    hardcode project name 'project' in manage.py
    Since we have a fixed project name now, there's no need for complex
    project folder discovery logic, so we can simplify things.

--- manage.py
+++ manage.py
@@ -1,25 +1,10 @@
 #!/usr/bin/env python
-from os import environ, listdir
-from os.path import join, dirname, abspath, exists
+import os
 import sys
 
 if __name__ == "__main__":
-
-    # Try to discover project name and set the default settings module
-    # based on it. If discovery fails, DJANGO_SETTINGS_MODULE environment
-    # variable must be set.
-
-    root = dirname(abspath(__file__))
-    sys.path.append(root)
-    settings_module = None
-    for name in listdir(root):
-        full_name = join(root, name)
-        if exists(join(full_name, 'settings', '__init__.py')):
-                settings_module = name + '.settings'
-                break
-
-    if settings_module is not None:
-        environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
     from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)

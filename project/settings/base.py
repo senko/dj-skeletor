573115d3b60d007625536e7ada2cf7bf6507153e
narsic <narsicnet@gmail.com>
2015-02-11 16:38:52 +0330

    Remove South from project apps

--- project/settings/base.py
+++ project/settings/base.py
@@ -135,7 +135,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'south',
     'djcelery',
     # OAUTH 2
     'provider',
